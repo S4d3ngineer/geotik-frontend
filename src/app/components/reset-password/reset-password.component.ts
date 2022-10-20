bd6fa9bd183cca92467270809b82fa3539701361
S4d3ngineer <adam.arkuszynski@gmail.com>
2022-10-21 01:48:11 +0200

    Make component submit to actual endpoint

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -10,5 +11,32 @@ export class ResetPasswordComponent {
   email: string | null = null;
 
   submitted = false;
+
+  message: string | null = null;
+
+  private resetPasswordUrl = 'http://localhost:9090/users/resetPassword';
+
+  // private httpOptions = {
+  //   headers: new HttpHeaders({
+  //     'Content-Type': 'application/json'
+  //   })
+  // } 
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  onSubmit(): void {
+    this.http.post<{msg: string}>(this.resetPasswordUrl, this.email)
+      .subscribe({
+        next: () => {
+          this.submitted = true;
+        },
+        error: e => {
+          console.error(e);
+          this.message = e.error.msg;
+        }
+      });
+  }
 
 }
